0adf0d61b8d3a806ee14edb7f24ce33d69a42484
david <drgrieve@hotmail.com>
2019-02-25 17:03:05 +1100

    emailaddresswebattribute

---
+++ src/ChilliSource.Cloud.Web.Tests/Attributes/EmailAddressWebAttributeTests.cs
@@ -0,0 +1,48 @@
+﻿using ChilliSource.Cloud.Web.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+using Xunit;
+
+namespace ChilliSource.Cloud.Web.Tests
+{
+    public class EmailAddressWebAttributeTests
+    {
+
+        [Theory]
+        [InlineData("joe@example.com", true)]
+        [InlineData("jane+12345@example.com.au", true)]
+        [InlineData("x@x.xx", true)]
+        [InlineData("example.com", false)]
+        [InlineData("@example.com", false)]
+        [InlineData("joe@example.com.", false)]
+        [InlineData("joe@example..com.au", false)]
+        [InlineData("joe..13@example.com", false)]
+        [InlineData("111111111111111111", false)]
+
+        public void EmailAddressWebAttribute_ShouldBeValidatedCorrectly_WhenEmailAddressIsGiven(string emailaddress, bool shouldBeCorrect)
+        {
+            var emailAddressWebAttribute = new EmailAddressWebAttribute();
+            if (shouldBeCorrect)
+            {
+                Assert.True(emailAddressWebAttribute.IsValid(emailaddress));
+            }
+            else
+            {
+                Assert.False(emailAddressWebAttribute.IsValid(emailaddress));
+            }
+        }
+
+        [Theory]
+        [InlineData("joe@example.com.")]
+        [InlineData("joe@example..com.au")]
+        public void EmailAddressWebAttribute_ShouldBeValidatedIncorrectly_WhenEdgeCaseEmailAddressIsGiven(string emailaddress)
+        {
+            var emailAddressAttribute = new EmailAddressAttribute();
+            Assert.True(emailAddressAttribute.IsValid(emailaddress));
+        }
+
+    }
+}
---
+++ src/ChilliSource.Cloud.Web/Attributes/EmailAddressWebAttribute.cs
@@ -0,0 +1,35 @@
+﻿using ChilliSource.Cloud.Core.Email;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChilliSource.Cloud.Web.Attributes
+{
+    /// <summary>
+    /// Validates email address properties. 
+    /// </summary>
+    public class EmailAddressWebAttribute : DataTypeAttribute
+    {
+
+        public EmailAddressWebAttribute()
+            : base(DataType.EmailAddress)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            var s = value as string;
+            if (String.IsNullOrEmpty(s)) return false;
+
+            if (base.IsValid(value))
+            {
+                var emailAddressAttribute = new EmailAddressAttribute();
+                if (emailAddressAttribute.IsValid(value))
+                {
+                    return s.IsValidEmailAddress();
+                }
+            }
+
+            return false;
+        }
+    }
+}
